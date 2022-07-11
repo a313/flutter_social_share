a1caa6abbd74cb3ebc2066bbcb30a2e8163b7bf9
tungnt <tungnt@techlab.ai>
2022-07-11 17:53:39 +0700

    add ios call

--- ios/Classes/SwiftFlutterSocialSharePlugin.swift
+++ ios/Classes/SwiftFlutterSocialSharePlugin.swift
@@ -1,14 +1,58 @@
 import Flutter
 import UIKit
+import FBSDKShareKit
 
-public class SwiftFlutterSocialSharePlugin: NSObject, FlutterPlugin {
-  public static func register(with registrar: FlutterPluginRegistrar) {
-    let channel = FlutterMethodChannel(name: "flutter_social_share", binaryMessenger: registrar.messenger())
-    let instance = SwiftFlutterSocialSharePlugin()
-    registrar.addMethodCallDelegate(instance, channel: channel)
-  }
-
-  public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
-    result("iOS " + UIDevice.current.systemVersion)
-  }
+public class SwiftFlutterSocialSharePlugin: NSObject, FlutterPlugin, SharingDelegate {
+    public func sharer(_ sharer: Sharing, didCompleteWithResults results: [String : Any]) {
+        
+    }
+    
+    public func sharer(_ sharer: Sharing, didFailWithError error: Error) {
+        
+    }
+    
+    public func sharerDidCancel(_ sharer: Sharing) {
+        
+    }
+    
+    public static func register(with registrar: FlutterPluginRegistrar) {
+        let channel = FlutterMethodChannel(name: "flutter_social_share", binaryMessenger: registrar.messenger())
+        let instance = SwiftFlutterSocialSharePlugin()
+        registrar.addMethodCallDelegate(instance, channel: channel)
+    }
+    
+    public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
+        if call.method == "shareToFacebook" {
+            if let arguments = call.arguments as? [String:Any] {                
+                var shareQuote = arguments["quote"] as! String
+                var shareUrl = arguments["url"] as! String
+                shareToFacebook(withQuote: shareQuote, withUrl:shareUrl)
+            }
+        }
+    }
+    var result: FlutterResult?
+    
+    private func shareToFacebook(withQuote quote: String?, withUrl urlString: String?) {
+        DispatchQueue.main.async {
+            let shareContent = ShareLinkContent()
+            if let url = urlString {
+                shareContent.contentURL = URL.init(string: url)!
+            }
+            if let quoteString = quote {
+                shareContent.quote = quoteString
+            }
+            if let flutterAppDelegate = UIApplication.shared.delegate as? FlutterAppDelegate {
+                let shareDialog = ShareDialog(
+                    viewController: flutterAppDelegate.window.rootViewController,
+                    content: shareContent,
+                    delegate: self
+                )
+                shareDialog.mode = .automatic
+                shareDialog.show()
+                self.result?("Success")
+            } else{
+                self.result?("Failure")
+            }
+        }
+    }
 }
--- ios/flutter_social_share.podspec
+++ ios/flutter_social_share.podspec
@@ -9,12 +9,14 @@ Pod::Spec.new do |s|
   s.description      = <<-DESC
 A new flutter plugin project.
                        DESC
-  s.homepage         = 'http://example.com'
+  s.homepage         = 'http://github.com/a313'
   s.license          = { :file => '../LICENSE' }
-  s.author           = { 'Your Company' => 'email@example.com' }
+  s.author           = { 'a313' => 'nguyenthanhtung0207@gmail.com' }
   s.source           = { :path => '.' }
   s.source_files = 'Classes/**/*'
   s.dependency 'Flutter'
+  s.dependency 'FBSDKCoreKit'
+  s.dependency 'FBSDKShareKit'
   s.platform = :ios, '9.0'
 
   # Flutter.framework does not contain a i386 slice.
