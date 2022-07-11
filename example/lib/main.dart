c3ccbe204645061f69b699501d021fccf87481f0
tungnt <tungnt@techlab.ai>
2022-07-11 17:54:38 +0700

    add example

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,7 +1,7 @@
-import 'package:flutter/material.dart';
-import 'dart:async';
+import 'dart:developer';
 
-import 'package:flutter/services.dart';
+import 'package:flutter/material.dart';
+
 import 'package:flutter_social_share/flutter_social_share.dart';
 
 void main() {
@@ -16,34 +16,9 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  String _platformVersion = 'Unknown';
-
   @override
   void initState() {
     super.initState();
-    initPlatformState();
-  }
-
-  // Platform messages are asynchronous, so we initialize in an async method.
-  Future<void> initPlatformState() async {
-    String platformVersion;
-    // Platform messages may fail, so we use a try/catch PlatformException.
-    // We also handle the message potentially returning null.
-    try {
-      platformVersion =
-          await FlutterSocialShare.platformVersion ?? 'Unknown platform version';
-    } on PlatformException {
-      platformVersion = 'Failed to get platform version.';
-    }
-
-    // If the widget was removed from the tree while the asynchronous platform
-    // message was in flight, we want to discard the reply rather than calling
-    // setState to update our non-existent appearance.
-    if (!mounted) return;
-
-    setState(() {
-      _platformVersion = platformVersion;
-    });
   }
 
   @override
@@ -54,9 +29,20 @@ class _MyAppState extends State<MyApp> {
           title: const Text('Plugin example app'),
         ),
         body: Center(
-          child: Text('Running on: $_platformVersion\n'),
+          child: TextButton(
+            child: const Text("Share on facebook"),
+            onPressed: () {
+              shareOnFacebook();
+            },
+          ),
         ),
       ),
     );
+  }
+
+  void shareOnFacebook() async {
+    String? result = await FlutterSocialShare.shareToFacebook(
+        url: "https://www.google.com", quote: "captions");
+    log(result ?? "");
   }
 }
